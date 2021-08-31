46e103df7cc645c754f3303c20346d5f2d844071
Ayaan-WHJ <78539077+Ayaan-WHJ@users.noreply.github.com>
2021-08-31 16:38:41 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 video = "";
 status = "";
-
+object = [];
 function preload()
 {
     video = createVideo("video.mp4");
@@ -16,6 +16,20 @@ function setup()
 function draw()
 {
     image(video,0,0,480,380);
+    if (status != "") {
+    object_detector.detect(video,gotResults);
+    }
+    for(i=0;i<object.length;i++)
+{
+document.getElementById("status").innerHTML = "Status - Detecting objects ";
+document.getElementById("number_of_objects").innerHTML = "Number of objects detected are : " + object.length;
+fill("#FF0000");
+percent = floor(object[i].confidence * 100);
+text(object[i].label + " " + percent + "%",object[i].x + 15, object[i].y +15);
+noFill();
+stroke("#FF0000");
+rect(object[i].x , object[i].y , object[i].width , object[i].height);
+}
 }
 
 function start()
@@ -31,4 +45,12 @@ function modelLoaded()
     video.loop();
     video.volume(0);
     video.speed(1);
+}
+function gotResults(error,results)
+{
+if (error) {
+    console.log(error);
+}
+console.log(results);
+object = results;
 }
